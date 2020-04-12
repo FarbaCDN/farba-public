4f88d9effdfb22aa390126245fd3790ee285c3ab
Anton Lakhtikov <mranton@Debian-99-stretch-64-minimal>
2020-04-12 06:27:38 +0200

    Handle relative URLs

--- js/farba-cdn.js
+++ js/farba-cdn.js
@@ -4,8 +4,15 @@ function change_url() {
   var images = document.getElementsByClassName("farba");
 
   for(var i = 0; i < images.length; i++) {
-    images[i].addEventListener("error", cdn_error);
-    images[i].src = "http://farba.io/u?" + images[i].dataset.src;
+    if (images[i].dataset.src.startsWith("./gallery/image.php") ||            //random image
+        images[i].dataset.src.startsWith("./download/file.php?avatar=") ||    //avatar
+        images[i].dataset.src.startsWith("./images/avatars")) {               //avatar
+
+      images[i].addEventListener("error", cdn_error);
+      images[i].src = "http://farba.io/u?" + window.location.origin + images[i].dataset.src.slice(1, ); //strip dot
+    } else {
+      images[i].src = images[i].dataset.src;
+    }
   }
 }
 
